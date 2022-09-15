be45d3f1b3fd7751f977e94fa52a12a80a1c60a4
bsodocean <socajowa3@gmail.com>
2022-09-15 10:43:29 +0100

    Added React-icons & Next-sanity to package.json, custom schemas

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -1,1 +1,11 @@
-    
+import type { NextApiRequest, NextApiResponse } from 'next/types'
+type Data = {
+    categories: Category[],
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    // const categories = await sanityClient.fetch(query)
+}
---
+++ sanity.js
@@ -0,0 +1,18 @@
+import { createClient } from "next-sanity";
+import createImageUrlBuilder from "@sanity/image-url";
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  token: process.env.SANITY_API_TOKEN,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: "2021-10-21",
+  useCdn: process.env.NODE_ENV === "production",
+};
+
+// Set up the client for fetching data in the getProps page functions
+
+export const sanityClient = createClient(config);
+
+// Set up helper function for generating image URLs with ony asset reference data in your documents.
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
